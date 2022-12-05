082481a51d81102091f21ad88abf71f1303ed36d
Lucas Mendes <lucaslsbh1@gmail.com>
2022-12-05 07:34:16 -0300

    [Day 04] - First part solution

---
+++ days/04/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+var (
+	//go:embed input.txt
+	input string
+)
+
+func main() {
+	var (
+		// 38-41,38-38
+		// 18-65,18-65
+		assignmentGroups = strings.Split(input, "\n")
+		result           = 0
+	)
+
+	for _, group := range assignmentGroups {
+		// [38-41 38-38]
+		elfAssignments := strings.Split(group, ",")
+
+		// 38-41
+		firstElfAssignment := NewAssignment(elfAssignments[0])
+		// 38-38
+		secondElfAssignment := NewAssignment(elfAssignments[1])
+
+		if firstElfAssignment.FullyOverlap(secondElfAssignment) || secondElfAssignment.FullyOverlap(firstElfAssignment) {
+			result += 1
+		}
+	}
+
+	fmt.Println("Result:", result)
+}
+
+type Assignment struct {
+	Raw   string
+	Start int
+	End   int
+}
+
+func NewAssignment(raw string) Assignment {
+	strStartAndEnd := strings.Split(raw, "-")
+
+	start, _ := strconv.Atoi(strStartAndEnd[0])
+	end, _ := strconv.Atoi(strStartAndEnd[1])
+
+	return Assignment{
+		Raw:   raw,
+		Start: start,
+		End:   end,
+	}
+}
+
+func (a Assignment) FullyOverlap(counterpartyAssignment Assignment) bool {
+	if a.Start == counterpartyAssignment.Start && a.End == counterpartyAssignment.End {
+		return true
+	}
+
+	if a.Start <= counterpartyAssignment.Start && a.End >= counterpartyAssignment.End {
+		return true
+	}
+
+	return false
+}
---
+++ days/04/main_test.go
@@ -0,0 +1,55 @@
+package main
+
+import "testing"
+
+func TestAssignment_FullyOverlap(t *testing.T) {
+	type fields struct {
+		Raw string
+	}
+	type args struct {
+		counterpartyAssignment Assignment
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   bool
+	}{
+		{
+			fields: fields{
+				Raw: "38-41",
+			},
+			args: args{
+				counterpartyAssignment: NewAssignment("38-38"),
+			},
+			want: true,
+		},
+		{
+			fields: fields{
+				Raw: "10-20",
+			},
+			args: args{
+				counterpartyAssignment: NewAssignment("12-18"),
+			},
+			want: true,
+		},
+		{
+			fields: fields{
+				Raw: "10-20",
+			},
+			args: args{
+				counterpartyAssignment: NewAssignment("20-25"),
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := NewAssignment(tt.fields.Raw)
+
+			if got := a.FullyOverlap(tt.args.counterpartyAssignment); got != tt.want {
+				t.Errorf("FullyOverlap() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
